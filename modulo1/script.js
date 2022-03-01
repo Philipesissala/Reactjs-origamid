134c499a9b6508ccb3aa27e2101748dc8c9fd413
Philipesissala <filipesissala14@gmail.com>
2022-03-01 15:00:17 +0100

    Começando os estudos de react

---
+++ modulo1/script.js
@@ -0,0 +1,36 @@
+import { areaQuadrado, perimetroQuadrado } from "./quadrado.js";
+console.log(areaQuadrado(51));
+
+/*const menu = {
+  seletor: ".principal",
+};
+
+console.log(menu.seletor);
+
+function upperName(name) {
+  return name.toUpperCase();
+}
+
+const upName = (name) => {
+  return name.toUpperCase();
+};
+
+console.log(upperName("filipe"));
+
+//Operador Rest
+function showList(empresa, ...clientes) {
+  clientes.forEach((cliente) => {
+    console.log(cliente, empresa);
+  });
+}
+
+showList(
+  "Google",
+  "Andre",
+  "Rafael",
+  "Antonio",
+  "Eduardo",
+  "Alberto",
+  "Miguel"
+);
+*/
