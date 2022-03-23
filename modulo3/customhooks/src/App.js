13352c9d77a7709ed76ea474a28740452727646f
Philipesissala <filipesissala14@gmail.com>
2022-03-23 19:32:47 +0100

    Exercicio de custom hooks 1

--- modulo3/customhooks/src/App.js
+++ modulo3/customhooks/src/App.js
@@ -1,6 +1,18 @@
 import React from "react";
+import useLocalStorage from "./useLocalStorage";
+
 function App() {
-  return <></>;
+  const [produto, setProduto] = useLocalStorage("produto", "");
+  function handleClick({ target }) {
+    setProduto(target.innerText);
+  }
+  return (
+    <>
+      <p>Produto preferido: {produto}</p>
+      <button onClick={handleClick}>notebook</button>
+      <button onClick={handleClick}>smartphone</button>
+    </>
+  );
 }
 
 export default App;
