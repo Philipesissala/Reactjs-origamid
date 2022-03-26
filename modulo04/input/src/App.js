706cd4cc312197b8ae4bb32244ee3ecde1e856dc
Philipesissala <filipesissala14@gmail.com>
2022-03-26 18:01:06 +0100

    exercitando input

--- modulo04/input/src/App.js
+++ modulo04/input/src/App.js
@@ -1,6 +1,32 @@
+import React from 'react';
+
 function App() {
+  const [nome, setNome] = React.useState('');
+  const [email, setEmail] = React.useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(event);
+  }
+  
   return (
     <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="nome"
+          value={nome}
+          onChange={(event) => setNome(event.target.value)}
+        />
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <button>Salvar</button>
+      </form>
+      {nome}
     </>
   );
 }
